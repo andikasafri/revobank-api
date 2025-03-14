ce09f73a085be91464c2677247c0c6baafebdccb
andikasafri <andika.saf3@gmail.com>
2025-03-15 02:08:28 +0700

    refactor: remove wait-for-it.sh from docker-compose and streamline entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Debugging: Check if nc is available
-if ! command -v nc &> /dev/null
-then
-    echo "nc could not be found"
-    exit 1
-fi
-
-# Wait for MySQL to be ready
-# until nc -z mysql-container 3306; do
-#   echo "Waiting for MySQL..."
-#   sleep 1
-# done
-
 # Run database migrations
 flask db upgrade
 
